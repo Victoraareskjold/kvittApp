68d3ed74af4890a0b76799d07822c95b2e0f3521
Victoraareskjold <victor.aaareskjold@gmail.com>
2023-09-22 18:17:00 +0200

    fix

--- src/screens/onboarding/SetupPhone.js
+++ src/screens/onboarding/SetupPhone.js
@@ -1,122 +1,118 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Text, View, TouchableOpacity, TextInput, KeyboardAvoidingView, Platform, Alert } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, TouchableOpacity, TextInput, Alert, Platform, KeyboardAvoidingView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from '@expo/vector-icons';
+import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha';
+import firebase from "firebase/compat/app";
 
 import Colors from "../../../Styles/Colors";
 import FontStyles from "../../../Styles/FontStyles";
 import ButtonStyles from "../../../Styles/ButtonStyles";
 import ContainerStyles from "../../../Styles/ContainerStyles";
-
-import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha';
 import { firebaseConfig } from '../../../firebase';
-import firebase from "firebase/compat/app";
 
-export default function SetupPhone({ route, navigation }) {
+export default function SetupPhone({ navigation, route }) {
 
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [confirmedPhoneNumber, setConfirmedPhoneNumber] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [fullPhoneNumber, setFullPhoneNumber] = useState('');
+  const [codeSent, setCodeSent] = useState(false);
+  const [code, setCode] = useState('');
+  const recaptchaVerifier = useRef(null);
+  const [verificationId, setVerificationId] = useState('');
 
-    const [codeSent, setCodeSent] = useState(false);
-    const [code, setCode] = useState('');
-    const [verificationId, setVerificationId] = useState(null);
-    const recaptchaVerifier = useRef(null); 
+  useEffect(() => {
+    const cleanedPhoneNumber = phoneNumber.replace(/[^\d]/g, '');
+    if (cleanedPhoneNumber.length === 8) setFullPhoneNumber('+47' + cleanedPhoneNumber);
+  }, [phoneNumber]);
+
+  useEffect(() => {
+    console.log('Full Phone Number:', fullPhoneNumber);
+  }, [fullPhoneNumber]);
+
+  const formatPhoneNumber = (number) => {
+    let cleaned = number.replace(/[^\d]/g, ''); 
+    if (cleaned.length > 3 && cleaned.length <= 6) cleaned = cleaned.replace(/(\d{3})(\d{1,3})/, '$1 $2');
+    else if (cleaned.length > 6) cleaned = cleaned.replace(/(\d{3})(\d{2})(\d{3})/, '$1 $2 $3');
+    return cleaned;
+  };
+
+  const sendVerification = async () => {
+    const cleanedPhoneNumber = phoneNumber.replace(/[^\d]/g, ''); // Renser telefonnummeret for å fjerne alle ikke-numeriske tegn
+    if (cleanedPhoneNumber.length !== 8) return Alert.alert('Feil', 'Ugyldig telefonnummer');
   
-    const sendVerification = () => {
-      const phoneProvider = new firebase.auth.PhoneAuthProvider(); 
-      phoneProvider
-          .verifyPhoneNumber(phoneNumber, recaptchaVerifier.current) 
-          .then
-            (setVerificationId);
-            setCodeSent(true);
-      setConfirmedPhoneNumber(phoneNumber); // Lagre telefonnummer før du nullstiller det.
-      setPhoneNumber(''); 
-    };    
-        
-    const confirmCode = () => {
-      const credential = firebase.auth.PhoneAuthProvider.credential(
-        verificationId,
-        code
-      );
-      firebase
-        .auth()
-        .signInWithCredential(credential)
-        .then((userCredential) => {
-          setCode('');
-    
-          const user = userCredential.user;
-          const uid = user.uid;
-          
-          const { firstName, lastName } = route.params;
-          
-          firebase
-            .firestore()
-            .collection('users')
-            .doc(uid)
-            .set({
-              firstName: firstName,
-              lastName: lastName,
-              phoneNumber: confirmedPhoneNumber // Oppdatert her
-            })
-            .then(() => {
-              console.log('User added to Firestore');
-            })
-            .catch((error) => {
-              console.error('Error adding user to Firestore: ', error);
-            });
-          
-          navigation.navigate("SetupCode", {
-            firstName: firstName,
-            lastName: lastName,
-            phoneNumber: confirmedPhoneNumber // Oppdatert her
-          });
-        })
-        .catch((error) => {
-          alert(error);
-        });
-    };    
+    const constructedFullPhoneNumber = '+47' + cleanedPhoneNumber;
+  
+    try {
+      const phoneProvider = new firebase.auth.PhoneAuthProvider();
+      const id = await phoneProvider.verifyPhoneNumber(constructedFullPhoneNumber, recaptchaVerifier.current);
+      setVerificationId(id);
+      setCodeSent(true);
+      setFullPhoneNumber(constructedFullPhoneNumber);
+    } catch (error) {
+      console.error('Error sending verification code: ', error);
+      Alert.alert('Feil', 'En feil oppsto ved sending av verifiseringskode.');
+    }
+  };  
 
-    return (
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : '0'}
-        style={[ContainerStyles.backgroundContainer, { paddingHorizontal: 24 }]}
-      >
-        <SafeAreaView style={{ backgroundColor: Colors.white, flex: 1 }}>
-          <FirebaseRecaptchaVerifierModal ref={recaptchaVerifier} firebaseConfig={firebaseConfig} />
-          <Ionicons name="chevron-back" size={24} color="black" style={{ marginBottom: 12 }} onPress={() => navigation.goBack()} />
-  
-          <Text style={FontStyles.header}>Legg inn Telefonnummer</Text>
-          <Text style={[FontStyles.body2, { marginBottom: 64 }]}>Skriv inn ditt telefonnummer</Text>
-  
-          {!codeSent ? (
-            <>
-              <TextInput
-                style={ButtonStyles.defaultPlaceholder}
-                placeholder="+47 123 45 678"
-                onChangeText={setPhoneNumber}
-                keyboardType="phone-pad"
-                autoComplete="tel"
-              />
-              <TouchableOpacity style={ButtonStyles.primaryBtn} onPress={sendVerification}>
-                <Text style={FontStyles.bigBtn}>Send kode</Text>
-              </TouchableOpacity>
-            </>
-          ) : (
-            <>
-              <TextInput
-                style={ButtonStyles.defaultPlaceholder}
-                placeholder="Verifiseringskode"
-                onChangeText={(text) => {
-                  setCode(text);
-                }}
-                keyboardType="number-pad"
-              />
-              <TouchableOpacity style={ButtonStyles.primaryBtn} onPress={confirmCode}>
-                <Text style={FontStyles.bigBtn}>Bekreft kode og gå videre</Text>
-              </TouchableOpacity>
-            </>
-          )}
-        </SafeAreaView>
-      </KeyboardAvoidingView>
-    );
-  }
+  const confirmCode = async () => {
+    if (code.length === 0) return Alert.alert('Feil', 'Vennligst skriv inn verifiseringskoden du mottok.');
+
+    try {
+      const credential = firebase.auth.PhoneAuthProvider.credential(verificationId, code);
+      const { user } = await firebase.auth().signInWithCredential(credential);
+      const { firstName, lastName } = route.params;
+
+      await firebase.firestore().collection('users').doc(user.uid).set({
+        firstName,
+        lastName,
+        phoneNumber: fullPhoneNumber
+      });
+
+      navigation.navigate("SetupCode", { firstName, lastName, phoneNumber: fullPhoneNumber });
+    } catch (error) {
+      console.error('Error confirming code: ', error);
+      Alert.alert('Feil', 'En feil oppsto. Vennligst prøv igjen senere.');
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={[ContainerStyles.backgroundContainer, { paddingHorizontal: 24 }]}>
+      <SafeAreaView style={{ backgroundColor: Colors.white, flex: 1 }}>
+        <FirebaseRecaptchaVerifierModal ref={recaptchaVerifier} firebaseConfig={firebaseConfig} />
+        <Ionicons name="chevron-back" size={24} color="black" onPress={() => navigation.goBack()} />
+
+        <Text style={FontStyles.header}>Legg inn Telefonnummer</Text>
+        <Text style={[FontStyles.body2, { marginBottom: 64 }]}>Skriv inn ditt telefonnummer</Text>
+
+        {!codeSent ? (
+          <>
+            <TextInput
+              style={ButtonStyles.defaultPlaceholder}
+              placeholder="123 45 678"
+              keyboardType="phone-pad"
+              onChangeText={(text) => setPhoneNumber(formatPhoneNumber(text))}
+              value={phoneNumber}
+              autoComplete="tel"
+            />
+            <TouchableOpacity style={ButtonStyles.primaryBtn} onPress={sendVerification}>
+              <Text style={FontStyles.bigBtn}>Send kode</Text>
+            </TouchableOpacity>
+          </>
+        ) : (
+          <>
+            <TextInput
+              style={ButtonStyles.defaultPlaceholder}
+              placeholder="Verifiseringskode"
+              keyboardType="number-pad"
+              onChangeText={setCode}
+              value={code}
+            />
+            <TouchableOpacity style={ButtonStyles.primaryBtn} onPress={confirmCode}>
+              <Text style={FontStyles.bigBtn}>Bekreft kode og gå videre</Text>
+            </TouchableOpacity>
+          </>
+        )}
+      </SafeAreaView>
+    </KeyboardAvoidingView>
+  );
+}
